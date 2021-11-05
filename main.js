ce9aa5e7c1fb7e5813b21788173216fd40d2d575
amadolucas <92124872+amadolucas@users.noreply.github.com>
2021-11-05 10:59:33 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 
-	name_of_the_student_array = [];
+	nameOfTheStudentArray = [];
 	
 	function submit()
 	{
         
-        var name_1 = document.getElementById("name_of_the_student_1").value;
-        var name_2 = document.getElementById("name_of_the_student_2").value;
-        var name_3 = document.getElementById("name_of_the_student_3").value;
-        var name_4 = document.getElementById("name_of_the_student_4").value;
+        var name1 = document.getElementById("nameOfTheStudent1").value;
+        var name2 = document.getElementById("nameOfTheStudent2").value;
+        var name3 = document.getElementById("nameOfTheStudent3").value;
+        var name4 = document.getElementById("nameOfTheStudent4").value;
 
-        name_of_the_student_array.push(name_1);
-        name_of_the_student_array.push(name_2);
-        name_of_the_student_array.push(name_3);
-        name_of_the_student_array.push(name_4);
+        name_of_the_student_array.push(name1);
+        name_of_the_student_array.push(name2);
+        name_of_the_student_array.push(name3);
+        name_of_the_student_array.push(name4);
 
-		console.log(name_of_the_student_array);
+		console.log(nameOfTheStudentArray);
 		
-        document.getElementById("display_name").innerHTML = name_of_the_student_array;
-        document.getElementById("submit_button").style.display = "none";
-        document.getElementById("sort_button").style.display = "inline-block";
+        document.getElementById("displayName").innerHTML = nameOfTheStudentArray;
+        document.getElementById("submitButton").style.display = "none";
+        document.getElementById("sortButton").style.display = "inline-block";
 
 	}
 
 function sorting()
 {
-	name_of_the_student_array.sort();
-    console.log(name_of_the_student_array);
-	document.getElementById("display_name").innerHTML = name_of_the_student_array;
+	nameOfTheStudentArray.sort();
+    console.log(nameOfTheStudentArray);
+	document.getElementById("displayName").innerHTML = nameOfTheStudentArray;
 }
